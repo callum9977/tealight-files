7492e6238b6dfa24c0024827bd2d10c57212634e
callum9977 <thecallum9977@gmail.com>
2015-08-18 14:39:25 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -6,6 +6,6 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-distance = 10
+distance = 50
 for n in range (0,distance):
   move()
