266775786cb8d0d5ac006ed2f947f81bd8975de8
callum9977 <thecallum9977@gmail.com>
2015-08-18 11:57:40 +0100

    Running logo/chessboardpy.py

--- logo/chessboardpy.py
+++ logo/chessboardpy.py
@@ -1,5 +1,8 @@
 from tealight.logo import move, turn
-def square(side):
-  for i in range(0,4):
-    move(side)
-    turn(90)
+def polygon(edges, size):
+  angle = 360.0 / edges
+  for i in range(0, edges):
+    move(size)
+    turn(angle)
+    
+polygon(360,100)
