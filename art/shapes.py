3b16b8b3dac087cfbdc4b41db2accd1ca062caf1
callum9977 <thecallum9977@gmail.com>
2015-08-19 10:13:11 +0100

    Running art/shapes.py

--- art/shapes.py
+++ art/shapes.py
@@ -2,7 +2,7 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 color("blue")
 
-box(250,150,100,100)
+box(260,150,100,100)
 circle(300,200,50)
 spot(300,200,20)
 
