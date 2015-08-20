c1ce44c10a3882a310da04c892e3179c06568ff8
callum9977 <thecallum9977@gmail.com>
2015-08-20 13:43:40 +0100

    Running art/Sweepinggrid.py

--- art/Sweepinggrid.py
+++ art/Sweepinggrid.py
@@ -46,6 +46,7 @@ def handle_mousedown(x, y):
   if get(mine, boxX, boxY)==1:
     color('red')
     box(boxX*60,boxY*60,50,50)
+    while True
   if get(mine, boxX, boxY)==0:
     color('white')
     box(boxX*60,boxY*60,50,50)
