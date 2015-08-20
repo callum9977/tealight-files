c7b0fe63c6b00ffe47a8df47bebbecf88afa0467
callum9977 <thecallum9977@gmail.com>
2015-08-20 10:17:59 +0100

    Running art/Sweepinggrid.py

--- art/Sweepinggrid.py
+++ art/Sweepinggrid.py
@@ -1,5 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 for i in range(0,9):
-  color("blue")
-  box(i*60, 10, 50, 50)
+  for i in range(0,9):
+    color("blue")
+    box(i*60, 10, 50, 50)
+  
