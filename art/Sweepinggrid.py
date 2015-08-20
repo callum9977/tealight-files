54a493d913ab68cf5ada8bbdf206a51aec0db93e
callum9977 <thecallum9977@gmail.com>
2015-08-20 10:31:16 +0100

    Running art/Sweepinggrid.py

--- art/Sweepinggrid.py
+++ art/Sweepinggrid.py
@@ -3,9 +3,6 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 for j in range(0,10):
   for i in range(0,10):
     box(i*60, j*60, 50, 50)
-    if ((i+j) % 2) != 1:
-      color("black")
-    else:
-      color("white")
+    
     
   
